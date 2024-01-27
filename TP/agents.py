83050be408aa0441de17a030d684984299467c5c
Akintomiwa Akin-Onigbinde <aakinonigbinde@gmail.com>
2024-01-27 12:21:42 +0000

    mod Token

--- TP/agents.py
+++ TP/agents.py
@@ -1,19 +1,20 @@
 from mesa import Agent
+from random import choice
 
 
 # Define agent classes
-class Token(Agent):
-    def __init__(self, unique_id, x, y, model):
-        super().__init__(unique_id, model)
-        # EV attributes
-        self.type = "Resource"
-        self.charge_rate = 0
-        self.pos = (x,y)
-        self.holder = None
+# class Token(Agent):
+#     def __init__(self, unique_id, x, y, model):
+#         super().__init__(unique_id, model)
+#         # EV attributes
+#         self.type = "Resource"
+#         self.charge_rate = 0
+#         self.pos = (x,y)
+#         self.holder = None
 
-    def step(self):
-        # Implement Token behavior here
-        pass
+#     def step(self):
+#         # Implement Token behavior here
+#         pass
 
 class Player(Agent):
     def __init__(self, unique_id, model, playertype):
@@ -23,29 +24,69 @@ class Player(Agent):
         self.score = 0
         self.deck = []
 
-    def place_house(self) -> None:
+    def select_token_from_deck(self):
+        # randomly select token from deck
+        selected = choice(self.deck)
+        return selected
+
+    def place_house_token(self, token) -> None:
+        # place house on random grid location 
+        target = self.model.grid.random
         pass
 
-    def receive_profit(self) -> None:
+    def receive_profit(self,token) -> int:
+        self.score += token.value
+        return self.score
         pass
 
-    def consult_player(self, ally) -> None: 
+    def consult_player(self, teammate) -> None: 
         pass
 
-    def house_decide_location(self) -> None:
-        pass
+    # def house_decide_location(self) -> None:
+    #     pass
 
     def step(self):
         # Implement Player behavior here
         self.receive_profit()
         pass
 
-class Card(Agent):
+
+# Primitives
+# Token, SmallResourceToken, LargeResourceToken, SmallWasteToken, SmallWasteToken, LargeHouseToken, SmallHouseToken, 
+
+class Token(Agent):
     def __init__(self, x, y, model):
         super().__init__(x, y, model)
+        # token attr 
+        self.type = None
+        self.value = None
+        self.pos = (x,y)
+        self.holder = None
 
     def step(self):
-        # Implement Card behavior here
+        # Implement Token behavior here
+        pass
+
+class SmallResourceToken(Token):
+    def __init__(self, x, y, model):
+        super().__init__(x, y, model)
+        # token attr 
+        self.type = "Resource"
+        self.value = 1
+
+    def step(self):
+        # Implement Token behavior here
+        pass
+
+class LargeResourceToken(Token):
+    def __init__(self, x, y, model):
+        super().__init__(x, y, model)
+        # token attr 
+        self.type = "Resource"
+        self.value = 3
+
+    def step(self):
+        # Implement Token behavior here
         pass
 
 class Gamemaster(Agent):
